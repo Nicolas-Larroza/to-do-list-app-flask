8f81c303bda8503875a6f73179cb7104c3db566b
potatoe-II <nicolaslarroza1@gmail.com>
2025-12-12 15:51:45 -0300

    something went wrong with this commit

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder="templates")
 
+todos = [{"task" : "sample todo", "done" : False}]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", todos=todos)
+@app.route("/add", methods=["POST"])
+def add():
+    todo = request.form[todo]
+    todos.append({"task": todo, "done" : False})
+    return redirect(url_for("index"))
 
+@app.route("/edit/<int:index>", methods=["GET", "POST"])
+def edit(index):
+    todo = todos[index]
+    if request.method == "POST":
+        todo['task'] = request.form["todo"]
+        return render_template(url_for('index'))
+    else: 
+        return render_template("edit.html", todo = todo, index = index)
 if __name__ == '__main__':
     app.run(debug=True)
